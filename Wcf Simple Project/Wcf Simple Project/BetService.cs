1af93bd850c98dcdf6e58ba3f2e9d934d0f85b9c
Gayane Khachatryan <kh.gaiane@gmail.com>
2017-04-25 19:39:51 +0400

    Added project to repo

---
+++ Wcf Simple Project/Wcf Simple Project/BetService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCFSimpleProject
+{
+    
+    public class BetService : IBetService
+    {
+        public List<double> CalculateSin(double[] x)
+        {
+            return x.Select(p => Math.Sin(p)).ToList();
+        }
+
+        public List<Person> GetPerson()
+        {
+            return new List<Person>
+            {
+                new Person
+                {
+                    Name = "John Smith",
+                    Age = 45
+                },
+
+                new Person
+                {
+                    Name = "Jane Austen",
+                    Age = 24
+                }
+            };
+        }
+
+        public string GetValue(int i)
+        {
+            return ("you have sent" + i);
+        }
+    }
+}
---
+++ Wcf Simple Project/Wcf Simple Project/IBetService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCFSimpleProject
+{
+    
+    [ServiceContract]
+    public interface IBetService
+    {
+        [OperationContract]
+        string GetValue(int i);
+
+        [OperationContract]
+        List<double> CalculateSin(double[] x);
+
+        [OperationContract]
+        List<Person> GetPerson();
+    }
+}
