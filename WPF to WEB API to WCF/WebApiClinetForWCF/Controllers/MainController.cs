2962525773ccde38dc370802a4ed34fe0e8a3afc
Gayane Khachatryan <kh.gaiane@gmail.com>
2017-04-26 23:05:30 +0400

    Added project

---
+++ WPF to WEB API to WCF/WebApiClinetForWCF/Controllers/MainController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApiClinetForWCF.Controllers
+{
+    public class MainController : ApiController
+    {
+        // GET api/main
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/main/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/main
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/main/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/main/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ WPF to WEB API to WCF/WpfBaseClientForWebApi/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfBaseClientForWebApi
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonname_Click(object sender, RoutedEventArgs e)
+        {
+            HttpClient client = new HttpClient();
+            
+            HttpResponseMessage response = client.GetAsync(@"http://localhost:50966/api/main").Result;
+            
+            string message = response.Content.ReadAsStringAsync().Result;
+           
+            JavaScriptSerializer jss = new JavaScriptSerializer();
+
+            string content = jss.Deserialize<string>(message);
+
+            textblockname.Text = content;
+        }
+    }
+}
